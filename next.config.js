45ae0a08b5c1651fb5dc4af38d3403d799668c5a
Nonso <nonsoanetoh@gmail.com>
2023-04-29 12:55:40 +0100

    my rtf-next-starter

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const KEYS_TO_OMIT = [
+  "webpackDevMiddleware",
+  "configOrigin",
+  "target",
+  "analyticsId",
+  "webpack5",
+  "amp",
+  "assetPrefix",
+];
+
+const withTM = require("next-transpile-modules")(["three"]);
+
+module.exports = (_phase, { defaultConfig }) => {
+  const plugins = [[withTM]];
+
+  const wConfig = plugins.reduce(
+    (acc, [plugin, config]) => plugin({ ...acc, ...config }),
+    {
+      ...defaultConfig,
+      ...nextConfig,
+    }
+  );
+
+  const finalConfig = {};
+  Object.keys(wConfig).forEach((key) => {
+    if (!KEYS_TO_OMIT.includes(key)) {
+      finalConfig[key] = wConfig[key];
+    }
+  });
+
+  return finalConfig;
+};
