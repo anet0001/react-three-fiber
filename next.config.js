728aadd86709026bf229e0bf723ec187b3c0523d
Nonso <nonsoanetoh@gmail.com>
2023-04-29 14:07:53 +0100

    created basic mesh and fixed compiling issue.

--- next.config.js
+++ next.config.js
@@ -2,36 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-
-const KEYS_TO_OMIT = [
-  "webpackDevMiddleware",
-  "configOrigin",
-  "target",
-  "analyticsId",
-  "webpack5",
-  "amp",
-  "assetPrefix",
-];
-
 const withTM = require("next-transpile-modules")(["three"]);
 
-module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [[withTM]];
-
-  const wConfig = plugins.reduce(
-    (acc, [plugin, config]) => plugin({ ...acc, ...config }),
-    {
-      ...defaultConfig,
-      ...nextConfig,
-    }
-  );
-
-  const finalConfig = {};
-  Object.keys(wConfig).forEach((key) => {
-    if (!KEYS_TO_OMIT.includes(key)) {
-      finalConfig[key] = wConfig[key];
-    }
-  });
-
-  return finalConfig;
-};
+module.exports = withTM();
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
+import { Canvas } from "@react-three/fiber";
 import Head from "next/head";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -12,9 +10,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Welcome</h1>
-      </main>
+      <section className="home-container">
+        <div id="canvas-container">
+          <Canvas style={{ outline: "2px solid red" }}>
+            <mesh>
+              <boxBufferGeometry />
+              <meshPhongMaterial color="teal" />
+            </mesh>
+          </Canvas>
+        </div>
+      </section>
     </>
   );
 }
